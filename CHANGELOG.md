1a23568a59726f1650013997e20ed493ef6452a6
david de leon <david.deleon@infotec.mx>
2021-04-16 12:44:48 -0500

    HU CDGC-79

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,9 @@
-# Changelog
-Todos los cambios notables a este proyecto seran documentados es este archivo.
+# Registro de cambios
+Todos los cambios notables de este proyecto se documentarán en este archivo.
 
-El formato esta basado en [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-mantenemos las versiones de acuerdo a [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+El formato se basa en [Keep a Changelog] (https://keepachangelog.com/en/1.0.0/),
+y este proyecto se adhiere a [Semantic Versioning] (https://semver.org/spec/v2.0.0.html).
 
-**Dada una versión numero MAJOR.MINOR.PATCH:** (aplica a partir de 2.0.0)
-
-- Se incrementara **MAJOR**, cuando se implemente un nuevo tipo de **formulario** (Diferente Subsector).
-- Se incrementa **MINOR**, cuando se agregue una o mas **nueva(s) funcionalidad(es) a la aplicación**
-- Se incrementa **PATCH**, cuando se implemente un **fix**.
-
-
-## [Released]
-
-## [1.0.0] 2021-10-03
-
+## [0.0.1] - 2021-04-16
+### Agregar
+- [HU-CDGC-79] (https://jira.sct.gob.mx/browse/CDGC-79) Se agrega la seccion Datos del Beneficiario
---
+++ src/components/AltaDeCaminos/DatosBeneficiario.vue
@@ -0,0 +1,84 @@
+<template>
+ 
+ <div class="col-md-12 mx-auto">
+    <div class="row">
+        <h3>Datos del Beneficiario</h3>
+        <hr class="red">
+        <form role="form">
+          <h3>Presidente Municipal o representante de la comunidad</h3>
+   <div class="form-group row">
+       <h3></h3>
+    <div class="col-sm-4">
+        <label class="control-label" for="region">Nombre o nombres</label>
+        <input class="form-control" id="region" placeholder="" type="text">
+    </div>
+    <div class="col-sm-4">
+        <label class="control-label" for="region">Primer apellido</label>
+        <input class="form-control" id="region" placeholder="" type="text">
+    </div>
+    <div class="col-sm-4">
+    <label class="control-label" for="region">Segundo apellido</label>
+    <input class="form-control" id="region" placeholder="" type="text">
+    </div>
+    
+    <br></br>
+    <br></br>
+    <tr>
+    <div class="col-sm-6">
+    <label class="control-label" for="region">RFC del Beneficiario</label>
+    <input class="form-control" id="region" placeholder="" type="text">
+    </div>
+    <div class="col-sm-6">
+    <label class="control-label" for="region">Número de cuenta</label>
+    <input class="form-control" id="region" placeholder="" type="number">
+    </div>
+    </tr>
+<br>
+<tr>
+<div class="col-sm-4">
+<label class="control-label" for="region">Núm. Exterior</label>
+<input class="form-control" id="region" placeholder="" type="text">
+</div>
+<div class="col-sm-4">
+<label class="control-label" for="region">Calle</label>
+<input class="form-control" id="region" placeholder="" type="text">
+</div>
+<div class="col-sm-4">
+<label class="control-label" for="region">Colonia</label>
+<input class="form-control" id="region" placeholder="" type="text">
+</div>
+</tr>
+<br></br>
+<tr>
+<div class="col-sm-4">
+<label class="control-label" for="region">Código postal</label>
+<input class="form-control" id="region" placeholder="" type="text">
+</div>
+<div class="col-sm-4">
+<label class="control-label" for="email-01">Tipo de calle</label>
+<select class="form-control" id="email-01">
+    <option>Calle</option>
+    <option>Boulevard</option>
+    <option>Avenida Principal</option>
+    <option>Privada</option>
+    <option>Ninguno</option>
+</select>
+</div>
+</tr>
+
+
+    </div>
+    </form>
+   </div>
+</div>
+
+ 
+
+</template>
+<script>
+
+export default 
+    {
+        name:"DatosBeneficiario"
+    };
+</script>
