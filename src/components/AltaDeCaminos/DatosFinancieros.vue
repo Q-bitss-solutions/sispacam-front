313a83f2a9e5d7bf65845d4c87bc253acc34bbfb
david.leon <david.leon@sng.biz>
2021-08-25 14:32:24 -0500

    Exportacion Excel SIA

--- src/components/AltaDeCaminos/DatosFinancieros.vue
+++ src/components/AltaDeCaminos/DatosFinancieros.vue
@@ -6,6 +6,9 @@
         <input type="date" id="start" name="trip-start" 
                v-model="fechac"
                >
+                <input type="date" id="start" name="trip-start"  v-if="tipo_camino == 'O'"
+               :disabled = "editmode" v-model="fechacE" 
+               > 
     </div>
     <div class="col-md-1">
         <button class="btn btn-default btn-sm" type="button" id="buscarObras" @click="submit">
@@ -17,55 +20,36 @@
             <h2>Consulta SIA</h2>
       </div>
     </div>
-
-
-    <div class="col-md-12 mx-auto">
-         <div class="row">
-            <h3>Clave Presupuestal</h3>
-        
-  
-    
-            <hr class="red">
-    
-        <!-- <div>
-      <el-button :loading="downloadLoading" style="margin:0 0 20px 20px;" type="primary" icon="el-icon-document" @click="handleDownload">
-        Export Excel
-      </el-button>
-    </div> -->
-
+<div id="app">
+    <div class="row">
+       <div class="col-md-12"> 
         <div class="row">
-        <div class="col-md-12">            
-              <ejs-grid   ref="grid"
-                    :dataSource="clave" 
-                    :allowPaging='false' 
-                    :allowSorting='false'
-                    :pageSettings='pageSettings'
-                    :allowFiltering='true'
-                    :allowTextWrap='true'
-                    :rowSelected='rowSelected'
-                    >
-                    <e-columns>
+            <h3>Clave Presupuestal</h3>
+            <hr class="red">
+    </div>
+        <ejs-grid ref='grid1' 
+        id='FirstGrid' 
+        :dataSource='clave'
+        
+        :toolbar='toolbarOptions' 
+        :pageSettings='pageSettings'
+        :allowExcelExport='true' 
+        :toolbarClick='toolbarClick'>
+            <e-columns>
                         <e-column field='mes'           headerText='Mes' ></e-column>
                         <e-column field='modificado'    headerText='Modificado' textAlign='right' format='C2'></e-column>
                         <e-column field='ejercido'      headerText='Ejercido' textAlign='right' format='C2'></e-column>
                         <e-column field='comprometido'  headerText='Comprometido' textAlign='right' format='C2' ></e-column>
                         <e-column field='pagado'        headerText='Pagado' textAlign='right' format='C2'></e-column>
                         <e-column field='disponible'    headerText='Disponible' textAlign='right' format='C2'></e-column>             
-                    </e-columns>          
-                </ejs-grid>  
-          
-
-               
-        </div>
-          
-      </div>
-
- 
-        </div>
-        <div class="row">
+                    </e-columns> 
+        </ejs-grid>
+       </div>
+    </div>
+    <div class="row">
             <h3>Pagos SIA</h3>
             <hr class="red">
-        </div>
+    </div>
     <div class="tab-pane">
         <div class="row">
             <div  class="col-md-4 form-group"  > 
@@ -73,7 +57,7 @@
                  <select v-model="mes" class="form-control" id="mes">
                     <option value="">Selecciona un mes...</option>
                     <option value="01">Enero</option>
-                    <option value="02">Febreo</option>
+                    <option value="02">Febrero</option>
                     <option value="03">Marzo</option>
                     <option value="04">Abril</option>
                     <option value="05">Mayo</option>
@@ -84,7 +68,7 @@
                     <option value="10">Octubre</option>
                     <option value="11">Noviembre</option>
                     <option value="12">Diciembre</option>
-         </select>
+                </select>
             </div>
             <div class="col-md-4 form-group">
                 <label class="control-label" for="nombreBenf">Beneficiario:</label>
@@ -96,20 +80,12 @@
             </div>        
         </div>
     </div>
-        
-        
-
-        <div class="row">
-        <div class="col-md-12 table-responsive">            
-              <ejs-grid  height='450px' width='100%' ref="grid"
-                    :dataSource="lista" 
-                    :allowPaging='true' 
-                    :allowSorting='true'
-                    :pageSettings='pageSettings'
-                    :allowFiltering='true'
-                    :allowTextWrap='true'
-                    >
-                    <e-columns>
+    
+    <ejs-grid ref='grid2' 
+    id='SecondGrid' 
+    :dataSource='lista' 
+    :allowExcelExport='true'>
+        <e-columns>
                         <e-column field='idpago'         headerText='id' :visible='flag' width=120></e-column>
                         <e-column field='rfc_benef'  headerText='RFC' width=150></e-column>
                         <e-column field='nombre'      headerText='Beneficiario' width=150></e-column>
@@ -118,37 +94,32 @@
                         <e-column field='f_elab'      headerText='Fecha Elab' width=150></e-column>
                         <e-column field='f_pago'      headerText='Fecha Pago' width=150></e-column>  
                         <e-column field='estatus'    headerText='Estatus' width=150></e-column>              
-                    </e-columns>          
-                </ejs-grid>  
-
-
-               
-        </div>
-          
-      </div>
-    </div>
+                    </e-columns>  
+    </ejs-grid>
     
-</table>  
-
-
+</div>
+</table> 
 </template>
-
 <script>
 import { NumericTextBoxPlugin } from "@syncfusion/ej2-vue-inputs";
 import { DropDownListPlugin } from "@syncfusion/ej2-vue-dropdowns";
 import { generarId } from '@/api/alta-camino';
 import Vue from "vue";
-import { required } from 'vuelidate/lib/validators'
-import { GridPlugin, Sort, Page} from '@syncfusion/ej2-vue-grids';
 import ButtonGrid  from '@/components/ButtonGrid'
 import { getCvepres, getSpago, getMescons} from '@/api/obras'
 import VueCurrencyFilter from 'vue-currency-filter'
+import excel from 'vue-excel-export'
+
+import { GridPlugin, Toolbar, ExcelExport, Sort, Page } from '@syncfusion/ej2-vue-grids';
 
 
 
+
+Vue.use(GridPlugin);
 Vue.use(DropDownListPlugin);
 Vue.use(NumericTextBoxPlugin);
-Vue.use(GridPlugin);
+Vue.use(excel);
+
 Vue.use(VueCurrencyFilter, {
   symbol: '$', // El símbolo, por ejemplo €
   thousandsSeparator: ',', // Separador de miles
@@ -161,7 +132,6 @@ Vue.use(VueCurrencyFilter, {
 
 
 export default {
-    
     name: 'DatosFinancieros',
     props: {
         edo:{
@@ -169,34 +139,44 @@ export default {
             iso:''
         },
     },
-    data(){
-                var f = new Date();
+  data() {
+       var f = new Date();
                 var dt =(f.getMonth() +1)
+                var dy =(f.getDate() -1)
                 if(dt <= 9){
                     dt = '0' + (f.getMonth() +1)
                 }else{
                 dt=(f.getMonth() +1)
                 }
+                /**/
+                if(dy <= 9){
+                    dy = '0' + (f.getDate() -1)
+                }else{
+                dy=(f.getDate() -1)
+                }
                 var ft =f.getFullYear() + '-' + 
                         dt + '-' + 
-                       (f.getDate() -1)        
-        return {
-            nombre:'',
-            apaterno:'',
-            amaterno:'',
-            rfc_benef:'',
-            clabe:'',
-            juridica:'',
-            calle:'',
-            next:'',
-            colonia:'',
-            cp:'',
-            tipocalle:'',
-            estatus:'',
-            editmode: false,
+                        dy 
+                var ftE =dy + dt + f.getFullYear() 
+                console.log("ft")
+                console.log(ft)
+                console.log("ftE")
+                console.log(ftE)
+         
+return {
+  /*fData: data.slice(0, 5),*/
+  /*sData: employeeData.slice(0, 5),*/
+  editmode: false,
+  tipo_camino: null,
             flag: false,
             lista:[],
+            
             clave:[],
+            tit1:[],
+            tit2:[],
+            nama1:'',
+            nama2:'',
+            tHeader:[],
             pageSettings: { pageCount: 5, pageSize: 20  },
             flagEdicion:true,
             usuario:'' ,
@@ -205,56 +185,13 @@ export default {
             benef:'',
             downloadLoading: false,
             fechac :ft,
-                }
-                
-    },
-        validations: {
-        nombre: {
-            required,
-
-        },
-        apaterno: {
-            required,
-        },
-        amaterno: {
-            required,
-        },
-        rfc_benef: {
-            required,
-        },
-        clabe: {
-            required,
-        },
-    },
-     provide: {
-        grid: [Sort, Page]
-    }, 
-    
-    methods:{ 
-    handleDownload() {
-      this.downloadLoading = true
-      const tHeader = ['Mes', 'Modificado', 'Ejercido', 'Comprometido', 'Pagado', 'Disponible']
-      const filterVal= ['mes', 'modificado', 'ejercido', 'comprometido', 'pagado', 'disponible']
-      const list = this.clave
-      const data = this.formatJson(filterVal, list)
-      excel.export_json_to_excel({
-          header: tHeader,
-          data,
-          filename: this.filename,
-          autoWidth: this.autoWidth,
-          bookType: this.bookType
-        })
-    },
-    formatJson(filterVal, jsonData) {
-        return jsonData.map(v => filterVal.map(j => {
-            if (j === 'timestamp') {
-          return parseTime(v[j])
-        } else {
-          return v[j]
-        }
-      }))
-    },
-        rowSelected: function(args) {
+            fechacE :ftE,
+            vname:'',
+  toolbarOptions: ['ExcelExport']
+};
+  },
+  methods: {
+      rowSelected: function(args) {
             let selectedrowindex = this.$refs.grid.getSelectedRowIndexes();  // Get the selected row indexes.
             //alert(selectedrowindex); // To alert the selected row indexes.
             let selectedrecords = this.$refs.grid.getSelectedRecords();  // Get the selected records.
@@ -275,52 +212,107 @@ export default {
             }else{
                  this.lista = await getSpago(this.fechac) 
             }
+            
 
         },
-        async populate () {
+
+  
+  async populate () {
             try{
                 let results  = []
                 let clave = null
+                let nama1 = null
+                let nama2 = null
+                let tit1 = null
+                let tit2 = null
                 if(this.fechac){
                     this.clave = await getCvepres(this.fechac) 
-                    this.lista = await getSpago(this.fechac)    
+                    this.fdata = await getCvepres(this.fechac) 
+                    this.lista = await getSpago(this.fechac)   
+                    this.sdata = await getSpago(this.fechac)  
+                    this.nama1 = this.fechacE + '-' + 'clave_presupuestal.xls'
+                    this.nama2 = this.fechacE + '-' + 'Pagos_SIA.xlsx'
+                    this.tit1 = ['', '', '',  '' ,'Calendario de Recursos SIA']
+                    this.tit2 = ['', '', '',  'Programa Presupuestario U004 “Mejora en la conectividad municipal a través de caminos rurales y carreteras alimentadoras”' ,'Reporte de pagos del SIA']
+                console.log("this.fechacE")
+                console.log(this.fechacE)
+                console.log("this.fechac")
+                console.log(this.fechac)
                 }
             }catch(e) {
                 console.log('error-->')
                 console.log(e)
             }
         },
-        async valbenef() {
-            // ...
-        
-            
-            //12
-            if (this.fisica_moral == "M" && this.rfc_benef.length != 12) {
-            alert('El RFC capturado no corresponde a una persona moral')
-            
-             
-            }
-            //13
-            if (this.fisica_moral == "F" && this.rfc_benef.length != 13 ) {
-            alert('El RFC capturado no corresponde a una persona fisica')
-             
-            }
+    toolbarClick : function (args) {
+      if (args.item.id === 'FirstGrid_excelexport') { // 'Grid_excelexport' -> Grid component id + _ + toolbar item name
+         console.log("this.nama2")
+            console.log(this.nama2)
+        let appendExcelExportProperties = {
+           
+            multipleExport: { type: 'NewSheet' },
+            fileName: this.nama2,
+            header: {
+               headerRows: 5,
+                rows: [
+                                        {
+                        cells:[{
+                            colSpan: 8,
+                            value: 'SUBSECRETARIA DE INFRAESTRUCTURA',
+                            style: {fontSize: 20, hAlign:'Center', bold: true}
+
+                        }],
+                     
+                    },
+                    { cells:[{
+                            colSpan: 8,
+                            value: 'DIRECCION GENERAL DE CARRETERAS',
+                            style: {fontSize: 20, hAlign:'Center', bold: true}
+
+                        }],
+                    },
+                    { cells:[{
+                            colSpan: 8,
+                            value: 'DIRECCION COORDINADORA DE CAMINOS RURALES Y ALIMENTADORES',
+                            style: {fontSize: 10, hAlign:'Center', bold: true}
+
+                        }],
+                    },
+                    { cells:[{
+                            colSpan: 8,
+                            value: 'Programa Presupuestario U004 “Mejora en la conectividad municipal a través de caminos rurales y carreteras alimentadoras”',
+                            style: {fontSize: 10, hAlign:'Center', bold: true}
+
+                        }],
+                    }
+                    ],
+
+            }, 
+        };
+        console.log("appendExcelExportProperties")
+        console.log(appendExcelExportProperties)
+
+        let firstGridExport = this.$refs.grid1.excelExport(appendExcelExportProperties, true);
+        firstGridExport.then((fData) => {
+            this.$refs.grid2.excelExport(appendExcelExportProperties, false, fData);
+        });
+    }
   },
-  submit() { 
+  
+ submit() { 
      this.populate()
   },
-
   editTemplateA () { 
             return { 
                 template:ButtonGrid,
             }    
         },
-editTemplateB () { 
+  editTemplateB () { 
             return { 
                 template:ButtonGrid,
             }    
         },
-         async DatosFinacieros(){   
+    async DatosFinacieros(){   
              this.$emit("show-error", false); 
              this.$v.$touch()
              if (this.$v.$invalid) {
@@ -356,32 +348,36 @@ editTemplateB () {
                     this.$emit("show-error",'Error al guardar beneficiario');                  
                 }
             }
-        }
-    },
-    computed:{
-        
+        },
+        },
+        computed:{
+       
         getCdate(){
             var currentDate = new Date();
             this.fechac=currentDate.getDate()+'/'+currentDate.getMonth()+1+'/'+ currentDate.getYear()
-            return this.fechac
-        }
-    },
-     created(){
-         
-        if(this.$route.params.obraId){
             this.editmode = true
             
-            
+            return this.fechac,
+             
+             this.editmode
+                    console.log('this.fechac')
+                    console.log(this.fechac)
+        }
+        
+        },
 
+     created(){
+        if(this.$route.params.obraId){
+            this.editmode = true
         }
         this.populate()
-        
-        
-    }
+    },
+  provide: {
+grid: [Toolbar, ExcelExport]
+  }
 }
+
 </script>
-
-
-<style scoped>
+<style>
 
 </style>
