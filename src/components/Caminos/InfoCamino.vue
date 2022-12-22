d0ba79d91e9b00264ff450f6f88b468b0ec49545
IPalos <ignaciopalos.r@gmail.com>
2022-12-22 16:57:55 -0600

    corregir id_localidad

---
+++ src/components/Caminos/InfoCamino.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="row">
+		<div class="row col-md-9">
+			<div class="col-md-6">Tipo de Camino</div>
+			<div class="col-md-6">{{ camino.tipo_camino }}</div>
+		</div>
+		<div class="row col-md-9">
+			<div class="col-md-6">Estado</div>
+			<div class="col-md-6">{{ camino.cve_agee }}</div>
+		</div>
+		<div class="row col-md-9">
+			<div class="col-md-6">Municipio</div>
+			<div class="col-md-6">{{ camino.icve_municipio }}</div>
+		</div>
+		<div class="row col-md-9">
+			<div class="col-md-6">Localidades</div>
+			<div class="col-md-6">{{ camino.localidades }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "InfoCamino",
+	props: {
+		camino: Object
+	}
+
+}
+</script>
--- src/views/EditarCamino.vue
+++ src/views/EditarCamino.vue
@@ -1,28 +1,9 @@
 <template>
   <div>
 
-    <hr class="red" />
-
     <h2>Edición del camino: {{ clave }}</h2>
 
-    <div class="row">
-      <div class="row col-md-9">
-        <div class="col-md-6">Tipo de Camino</div>
-        <div class="col-md-6">{{ camino.tipo_camino }}</div>
-      </div>
-      <div class="row col-md-9">
-        <div class="col-md-6">Estado</div>
-        <div class="col-md-6">{{ camino.cve_agee }}</div>
-      </div>
-      <div class="row col-md-9">
-        <div class="col-md-6">Municipio</div>
-        <div class="col-md-6">{{ camino.icve_municipio }}</div>
-      </div>
-      <div class="row col-md-9">
-        <div class="col-md-6">Localidades</div>
-        <div class="col-md-6">{{ camino.localidades }}</div>
-      </div>
-    </div>
+    <InfoCamino :camino="camino"></InfoCamino>
 
     <hr class="red" />
 
@@ -34,16 +15,14 @@
       </div>
     </div>
 
-    <ModalSCT v-if="showModal">
-      <h3 slot="header">Agregar Beneficiario de Obra</h3>
-      <FormAgregarBeneficiario @updateFrentes="GetConvenios(camino.id)" v-on:closeModal="showModal=false" slot="body" :camino="camino">
-      </FormAgregarBeneficiario>
-    </ModalSCT>
 
     <hr class="red" />
 
+    <!-- Tabla Frentes de camino -->
+    <!-- TODO: Rebuild - Cambiar nombre del componente -->
     <div class="row">
-      <tabla-frentes-camino @deleteBeneficiario="GetConvenios(camino.id)" :frentes="this.convenios"></tabla-frentes-camino>
+      <tabla-frentes-camino @deleteBeneficiario="GetConvenios(camino.id)"
+        :frentes="this.convenios"></tabla-frentes-camino>
     </div>
 
     <hr class="red" />
@@ -54,6 +33,14 @@
       </div>
     </div>
 
+    <!-- Modal: Formulario Agregar Beneficiarios -->
+    <ModalSCT v-if="showModal">
+      <h3 slot="header">Agregar Beneficiario de Obra</h3>
+      <FormAgregarBeneficiario @updateFrentes="GetConvenios(camino.id)" v-on:closeModal="showModal=false" slot="body"
+        :camino="camino">
+      </FormAgregarBeneficiario>
+    </ModalSCT>
+
   </div>
 </template>
 
@@ -63,38 +50,17 @@ import ModalSCT from "../components/Modals/SCTModal.vue";
 import FormAgregarBeneficiario from "../components/Caminos/FormAgregarBeneficiario.vue";
 import { getupdate, listBeneficiariosCamino, listMetricasBeneficiario } from "../api/alta-camino";
 import AgregadoFrentes from "../components/Caminos/AgregadoFrentes.vue";
+import InfoCamino from '../components/Caminos/InfoCamino.vue';
 export default {
-  components: { TablaFrentesCamino, ModalSCT, FormAgregarBeneficiario, AgregadoFrentes },
+  components: { TablaFrentesCamino, ModalSCT, FormAgregarBeneficiario, AgregadoFrentes, InfoCamino },
   name: "EditarCamino",
   data() {
     return {
       camino: null,
       showModal: false,
-      metrics:[],
-
-      // TODO: Quitar endpoint harcodeadp
-      icve_municipio: "San Juan Teita",
-      localidades:
-        'LAS PILAS, EL TERCO, EL TORITO',
-      consecutivo: "2",
-      tipo_camino: "C",
-      nombre_camino: "CAMINOS DE OAXACA dlm",
-      longitud: "2.0",
-      longitud_pavimentar: "2.0",
-      ancho_camino: 2,
-      ubicacion_camino: "sureste",
-      caracteristicas: "ctcc",
-      beneficios: "benfc",
-      ciit: true,
-      tren_maya: false,
-      caminos_originales: false,
-      fecha: "2021-09-23T01:49:49.762Z",
-      clave: this.$route.params.obraId,
-      iso: "OAX",
-      usuarios: null,
-      estatus: "C",
-      archivo: "",
+      metrics: [],
       convenios: {},
+      clave: this.$route.params.obraId,
     };
   },
 
@@ -102,8 +68,6 @@ export default {
     async GetCamino(clave) {
       const response = await getupdate(clave);
       this.camino = response;
-      console.log("CAMINO");
-      console.log(this.camino);
       this.GetConvenios(response.id)
       this.GetMetrics(response.id)
     },
@@ -113,14 +77,11 @@ export default {
     },
 
     async GetConvenios(id_camino) {
-      console.log("GetConvenios");
       const response = await listBeneficiariosCamino(id_camino);
       this.convenios = response
     },
-    async GetMetrics(id_camino){
+    async GetMetrics(id_camino) {
       const response = await listMetricasBeneficiario(id_camino);
-      console.log("METRICS");
-      console.log(this.metrics);
       this.metrics = response
     }
 
