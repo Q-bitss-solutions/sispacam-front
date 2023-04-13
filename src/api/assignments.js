52465d3c9cb8b050db8ac74685b94403f44c6189
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-04-13 14:48:48 -0600

    add service for fetch assignment by id

---
+++ src/api/assignments.js
@@ -0,0 +1,8 @@
+import request from '@/utils/request'
+
+export function fetchAssignmentById(assignmentId){
+	return request({
+		url: `/residentes/asignaciones/${assignmentId}`,
+		method: 'GET',
+	})
+}
