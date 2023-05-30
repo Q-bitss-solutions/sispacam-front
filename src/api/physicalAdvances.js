76c66e886311aff6ee53922621915198cd0a4d24
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-05-30 17:35:02 -0600

    feat: add services for get and update physical advances

--- src/api/physicalAdvances.js
+++ src/api/physicalAdvances.js
@@ -3,8 +3,23 @@ import { mapPhysicalAdvancePayload } from '@/utils/payloadMappers'
 
 export function storePhysicalAdvanceToAssignment({ assignmentId, physicalAdvance }){
 	return request({
-		url: `/avance-fisico/${assignmentId}/`,
+		url: `/asignaciones/${assignmentId}/avance-fisico/`,
 		method: 'POST',
+		data: mapPhysicalAdvancePayload(physicalAdvance)
+	})
+}
+
+export function fetchPhysicalAdvanceById(physicalAdvanceId){
+	return request({
+		url: `/avance-fisico/${physicalAdvanceId}/`,
+		method: 'GET',
+	})
+}
+
+export function putPhysicalAdvance({ physicalAdvance, physicalAdvanceId }){
+	return request({
+		url: `/avance-fisico/${physicalAdvanceId}/`,
+		method: 'PUT',
 		data: mapPhysicalAdvancePayload(physicalAdvance)
 	})
 }
