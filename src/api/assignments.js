e5e3cf33af1cc275b5dc85a65463c36b4181fecb
victor-itg <victor-itg@users.noreply.github.com>
2023-05-31 04:46:05 +0000

    Deploying to gh-pages from @ Q-bitss-solutions/sispacam-front@80ffe96d993199a85dd1d3a097806d3eee9b8991 🚀

--- src/api/assignments.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchAssignmentById(assignmentId){
-	return request({
-		url: `/residentes/asignaciones/${assignmentId}/`,
-		method: 'GET',
-	})
-}
--- src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-const typeRoads = {
-  'A': 'Agencia',
-  'C': 'Cabecera',
-  'O': 'Otro',
-}
-
-export { typeRoads }
